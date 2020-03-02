f7a0264a7cad51cd9e0c5bfdd30e22644e8039ad
Qixiang <qixiang100@gmail.com>
2020-03-02 12:38:51 -0500

    added canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles/index.scss";
+
+const canvas = document.getElementById('canvas');
+
+canvas.width = 800;
+canvas.height = 800;
+
+const ctx = canvas.getContext("2d"); //magic paint brush that creates 2d elements
+
+ctx.fillRect(50, 50, 50, 50);
+ctx.fillRect(705, 50, 50, 50);
