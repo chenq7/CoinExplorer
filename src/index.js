6458e964840289f56044ef38066f5f2151ffa83b
Qixiang <qixiang100@gmail.com>
2020-03-02 18:02:03 -0500

    Refactored code and created basic layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./styles/index.scss";
+import CoinExplorer from './game';
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('game-canvas');
 
-canvas.width = 800;
-canvas.height = 800;
+let game = new CoinExplorer(canvas);
 
-const ctx = canvas.getContext("2d"); //magic paint brush that creates 2d elements
 
-ctx.fillRect(50, 50, 50, 50);
-ctx.fillRect(705, 50, 50, 50);
+// const ctx = canvas.getContext("2d"); //magic paint brush that creates 2d elements
+
+// ctx.fillRect(X, Y, W, H)
+
---
+++ src/level.js
@@ -0,0 +1,1 @@
+// array size 36 x 28,      25 x 25 pixels per block
