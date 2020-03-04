9509f1988b5bc072133659dae249177dcbb9bd70
Qixiang <qixiang100@gmail.com>
2020-03-04 01:29:38 -0500

    updated tile collision

--- src/character.js
+++ src/character.js
@@ -9,10 +9,32 @@ export default class Character {
     this.x_velocity = 0;
     this.y_velocity = 5;
     this.position = { x: startX, y: startY }
+    this.prevPosition = { x: startX, y: startY }
     this.characterImage = new Image();
     this.characterImage.src = "./src/images/wizard/wizard/1_IDLE_000.png";
     this.flipped = false;
   }
+
+  getLeft() { return this.position.x }
+  getRight() { return this.position.x + this.characterWidth }
+  getTop() { return this.position.y }
+  getBottom() { return this.position.y + this.characterHeight }
+
+  getPrevLeft() { return this.prevPosition.x }
+  getPrevRight() { return this.prevPosition.x + this.characterWidth }
+  getPrevTop() { return this.prevPosition.y }
+  getPrevBottom() { return this.prevPosition.y + this.characterHeight }
+
+  setLeft(x) { this.position.x = x}
+  setRight(x) { this.position.x = x - this.characterWidth }
+  setTop(y) { this.position.y = y }
+  setBottom(y) { this.position.y = y - this.characterHeight }
+
+  setPrevLeft(x) { this.prevPosition.x = x }
+  setPrevRight(x) { this.prevPosition.x = x - this.characterWidth }
+  setPrevTop(y) { this.prevPosition.y = y }
+  setPrevBottom(y) { this.prevPosition.y = y - this.characterHeight }
+
 
   renderCharacter(ctx) {
     ctx.drawImage(
@@ -52,79 +74,105 @@ export default class Character {
     }
   }
 
-  bounds() {
-    return {
-      left: this.x,
-      right: this.x + CONSTANTS.BIRD_WIDTH,
-      top: this.y,
-      bottom: this.y + CONSTANTS.BIRD_HEIGHT
-    };
-  }
-
   update(board, tiles, coins) {
+    this.prevPosition.y = this.position.x;
+    this.prevPosition.y = this.position.y;
+    
     this.position.x += this.x_velocity;
     this.position.y += this.y_velocity;
     
-    let left = this.position.x;
-    let right = this.position.x + this.characterWidth
-    let top = this.position.y;
-    let bottom = this.position.y + this.characterHeight;
-    
-    // Check map edges
-    if (left < 0) this.position.x = 0;
-    if (right > this.gameWidth)
-      this.position.x = this.gameWidth - this.characterWidth;
-    if (bottom > this.gameHeight)
-      this.position.y = this.gameHeight - this.characterHeight;
-    if (top < 0) this.position.y = 0;
-
-    // Check for tile collision
-    // check for the four sides, if it hits a tile, reset position
-
-    // 600, 665
-    // handle down
-    let charLeft = Math.floor(left / 40);
-    let charRight = Math.floor(right / 40);
-    let charTop = Math.floor(top / 40);
-    let charBottom = Math.floor(bottom / 40);
-
-    // if (this.position.x < charLeft * 40 && board[charBottom-1][charLeft] instanceof Object && 
-    //   board[charBottom-1][charLeft].constructor.name === 'Tile'){
-    //   this.position.x = charLeft * 40;
-    // }
-    // if (this.position.x > charRight * 40 && board[charBottom-1][charRight] instanceof Object &&
-    //   board[charBottom-1][charRight].constructor.name === 'Tile') {
-    //   this.position.x = charRight * 40 - 40;
-    // }
-    // if (this.position.y > charTop * 40 && board[charTop][charLeft+1] instanceof Object &&
-    //   board[charTop][charLeft+1].constructor.name === 'Tile') {
-    //   this.position.y = charTop * 40;
-    // }
-    // if (this.position.y < charBottom * 40 && board[charBottom][charLeft+1] instanceof Object &&
-    //   board[charBottom][charLeft+1].constructor.name === 'Tile') {
-    //   this.position.y = charBottom * 40 - 55;
-    // }
+    this.handleEdgeCollision();
 
     // handle tile collision 
-    // for (let i=0; i < tiles.length; i++){
-    //   if (this.position.x < coins[i].position.x + coins[i].width){
+    let left;
+    let right;
+    let top;
+    let bottom;
+    let value;
 
-    //   }
-    // }
+    top = Math.floor(this.getTop() / 40);
+    left = Math.floor(this.getLeft() / 40);
+    value = board[top][left];
+    this.handleTileCollision(value, left * 40, top * 40, 40);
 
+    top = Math.floor(this.getTop() / 40);
+    right = Math.floor(this.getRight() / 40);
+    value = board[top][right];
+    this.handleTileCollision(value, right * 40, top * 40, 40);
 
-    // handle coin collision
-    for (let i=0; i < coins.length; i++) {
+    bottom = Math.floor(this.getBottom()/ 40);
+    left = Math.floor(this.getLeft() / 40);
+    value = board[bottom][left];
+    this.handleTileCollision(value, left * 40, bottom * 40, 40);
+
+    bottom = Math.floor(this.getBottom() / 40);
+    right = Math.floor(this.getRight() / 40);
+    value = board[bottom][right];
+    this.handleTileCollision(value, right * 40, bottom * 40, 40);
+
+    return this.handleCoinCollision(coins);
+    
+  }
+
+  handleEdgeCollision() {
+    if (this.getLeft() < 0) this.setLeft(0);
+    if (this.getRight() > this.gameWidth) this.setRight(this.gameWidth);
+    if (this.getTop() < 0) this.setTop(0);
+    if (this.getBottom() > this.gameHeight) this.setBottom(this.gameHeight);
+  }
+
+  handleCoinCollision(coins) {
+    for (let i = 0; i < coins.length; i++) {
       if (
         this.position.x < coins[i].position.x + coins[i].width &&
         this.position.x + coins[i].width > coins[i].position.x &&
         this.position.y < coins[i].position.y + coins[i].height &&
         this.position.y + this.characterHeight > coins[i].position.y) {
-        debugger
         coins.splice(i, 1);
       }
     }
-
     return coins;
+  }
+
+  handleTileCollision(value, tileX, tileY, tileSize){
+    if (value instanceof Object && value.constructor.name === 'Tile'){
+      debugger
+      if (this.topTileCollision(tileY)) return;
+      if (this.leftTileCollision(tileX)) return;
+      if (this.rightTileCollision(tileX + tileSize)) return;
+      if (this.bottomTileCollision(tileY + tileSize)) return;
+    }
+  }
+
+  bottomTileCollision(bottomTile){
+    if (this.getTop() < bottomTile && this.getPrevTop() >= bottomTile){
+      this.setTop(bottomTile);
+      return true;
+    }
+    return false;
+  }
+
+  leftTileCollision(leftTile){
+    if (this.getRight() > leftTile && this.getPrevRight() <= leftTile){
+        this.setRight(leftTile - 0.01);
+        return true; 
+    }
+    return false;
+  }
+
+  rightTileCollision(rightTile){
+    if (this.getLeft() < rightTile && this.getPrevLeft() >= rightTile){
+      this.setLeft(rightTile);
+      return true;
+    }
+    return false;
+  }
+
+  topTileCollision(topTile){
+    if (this.getBottom() > topTile && this.getPrevBottom() <= topTile){
+        this.setBottom(topTile - 0.01);
+        return true;
+    }
+    return false;
   }
 }
